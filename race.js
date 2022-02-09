8dbf88e04f356ff1b73d9d6220e2538d61bdfdc2
Yarik <studio_h@ukr.net>
2022-02-09 19:12:15 +0200

    Release 2 function (moveRight) changed

--- race.js
+++ race.js
@@ -1,12 +1,12 @@
 
-var Car = function (x, y) {
+const Car = function (x, y) {
   this.x = x;
   this.y = y;
   this.draw();
 };
 
 Car.prototype.draw = function () {
-  var carHtml = '<img src="http://nostarch.com/images/car.png">';
+  const carHtml = '<img src="http://nostarch.com/images/car.png">';
   this.carElement = $(carHtml);
   this.carElement.css({
     position: "absolute",
@@ -22,23 +22,23 @@ Car.prototype.moveRight = function (speed,) {
     left: this.x,
     top: this.y
   });
-  if (this.x === 400) {
+  if (this.x > 450) {
     clearInterval(id1)
     clearInterval(id2)
   }
 };
 
-var car1 = new Car(10, 20);
-var car2 = new Car(10, 150);
+const car1 = new Car(10, 20);
+const car2 = new Car(10, 150);
 
 
 
-var id1 = setInterval(() => {
-  car1.moveRight(3)
+const id1 = setInterval(() => {
+  car1.moveRight(10)
 }, 30);
 
-var id2 = setInterval(() => {
-  car2.moveRight(3)
+const id2 = setInterval(() => {
+  car2.moveRight(10)
 }, 30);
 
 
