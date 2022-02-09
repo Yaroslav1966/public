c1e47ab436858b078b0095edb209a83b3edaea0f
Yarik <studio_h@ukr.net>
2022-02-09 19:35:44 +0200

    No Class

--- race.js
+++ race.js
@@ -1,34 +1,32 @@
 
-class Car {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.draw();
-  }
-  draw() {
-    const carHtml = '<img src="http://nostarch.com/images/car.png">';
-    this.carElement = $(carHtml);
-    this.carElement.css({
-      position: "absolute",
-      left: this.x,
-      top: this.y
-    });
-    $("body").append(this.carElement);
-  }
-  moveRight(speed) {
-    this.x += speed = Math.floor(Math.random() * speed);
-    this.carElement.css({
-      left: this.x,
-      top: this.y
-    });
-    if (this.x > 450) {
-      clearInterval(id1);
-      clearInterval(id2);
-    }
-  }
-}
+const Car = function (x, y) {
+  this.x = x;
+  this.y = y;
+  this.draw();
+};
 
+Car.prototype.draw = function () {
+  const carHtml = '<img src="http://nostarch.com/images/car.png">';
+  this.carElement = $(carHtml);
+  this.carElement.css({
+    position: "absolute",
+    left: this.x,
+    top: this.y
+  });
+  $("body").append(this.carElement);
+};
 
+Car.prototype.moveRight = function (speed,) {
+  this.x += speed = Math.floor(Math.random() * speed);
+  this.carElement.css({
+    left: this.x,
+    top: this.y
+  });
+  if (this.x > 450) {
+    clearInterval(id1)
+    clearInterval(id2)
+  }
+};
 
 const car1 = new Car(10, 20);
 const car2 = new Car(10, 150);
